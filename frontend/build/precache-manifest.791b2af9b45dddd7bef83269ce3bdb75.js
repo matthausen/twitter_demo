1d757f9a27d6da5fa223d8b7b5e3d8a4de081af1
Matteo Fusilli <matteo.fusilli@adarga.ai>
2019-10-04 10:15:23 +0100

    flask API

---
+++ frontend/build/precache-manifest.791b2af9b45dddd7bef83269ce3bdb75.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f3306b93cf733376f00dff8e9af6483",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ece9ad8808ad84f0e6f3",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "95bb0c66a75e79b7edc4",
+    "url": "/static/js/2.233f4f10.chunk.js"
+  },
+  {
+    "revision": "ece9ad8808ad84f0e6f3",
+    "url": "/static/js/main.3f9fa55d.chunk.js"
+  },
+  {
+    "revision": "aa5dd861c3df341bfb4f",
+    "url": "/static/js/runtime-main.0fda84ed.js"
+  }
+]);
