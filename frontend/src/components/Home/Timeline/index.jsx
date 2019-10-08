7ac8d35dcfeda4ae14ef57b8e147f0918c5318e5
Matteo Fusilli <matteo.fusilli@adarga.ai>
2019-10-08 16:24:26 +0100

    fetch followers and following list

--- frontend/src/components/Home/Timeline/index.jsx
+++ frontend/src/components/Home/Timeline/index.jsx
@@ -7,6 +7,7 @@ import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Title from '../title';
+import Divider from '@material-ui/core/Divider';
 import { Typography } from '@material-ui/core';
 
 const useStyles = makeStyles(theme => ({
@@ -20,16 +21,21 @@ const apiEndpoint = 'http://127.0.0.1:5000/';
 export default function TimeLine(props) {
   const classes = useStyles();
   const [followers, setFollowers] = useState();
+  const [friends, setFriends] = useState();
 
   useEffect(() => {
     fetchFollowing();
+    fetchFriends();
   }, []);
 
   async function fetchFollowing() {
     await axios.get(apiEndpoint)
-      .then(res => {
-        setFollowers(res.data);
-      })
+      .then(res => setFollowers(res.data))
+  }
+
+  async function fetchFriends() {
+    await axios.get(apiEndpoint)
+      .then(res => setFriends(res.data))
   }
 
   return (
@@ -59,7 +65,35 @@ export default function TimeLine(props) {
         </div>
       ) : (
           <div className={classes.seeMore}>
-            No tweets
+            No followers
+      </div>
+        )}
+      <Divider />
+      {friends ? (
+        <div>
+          <Table size="small">
+            <TableHead>
+              <TableRow>
+                <TableCell>Following</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {friends.map((f, index) => (
+                <TableRow key={index}>
+                  <TableCell>{f}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+          <div className={classes.seeMore}>
+            <Typography color="primary">
+              See more followers
+          </Typography>
+          </div>
+        </div>
+      ) : (
+          <div className={classes.seeMore}>
+            No friends
       </div>
         )}
     </React.Fragment>
